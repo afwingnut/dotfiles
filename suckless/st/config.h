9d4b8efc3ad94d38010fdd2787f024f60c60e07e
afwingnut <afwingnut@gmail.com>
2023-11-09 05:04:58 -0500

    delete dwm changes

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font    = "Monofur Nerd Font:pixelsize=16:antialias=true:autohint=true";
+static char *font    = "Monofur Nerd Font:pixelsize=20:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"Noto Color Emoji:pixelsize=16:antialias=true:autohint=true",
